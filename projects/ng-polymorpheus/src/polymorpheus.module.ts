ddeaf3679b1d9794eeed21dff37cefcf3af7075c
waterplea <alexander@inkin.ru>
2021-05-13 22:55:10 +0300

    chore(deps): bump angular version

--- projects/ng-polymorpheus/src/polymorpheus.module.ts
+++ projects/ng-polymorpheus/src/polymorpheus.module.ts
@@ -1,10 +1,8 @@
-import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {PolymorpheusOutletDirective} from './directives/outlet';
 import {PolymorpheusTemplate} from './directives/template';
 
 @NgModule({
-    imports: [CommonModule],
     declarations: [PolymorpheusOutletDirective, PolymorpheusTemplate],
     exports: [PolymorpheusOutletDirective, PolymorpheusTemplate],
 })
