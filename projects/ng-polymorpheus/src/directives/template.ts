d86d20070263a98a6e671100b01dc8476c4310b8
splincode <omaxphp@yandex.ru>
2024-06-12 23:53:05 +0300

    fix: inheritance compatibility

--- projects/ng-polymorpheus/src/directives/template.ts
+++ projects/ng-polymorpheus/src/directives/template.ts
@@ -10,17 +10,12 @@ import {ChangeDetectorRef, Directive, inject, TemplateRef} from '@angular/core';
     exportAs: 'polymorpheus',
 })
 export class PolymorpheusTemplate<C = any> {
-    private readonly cdr;
-    public readonly template: TemplateRef<C>;
     public polymorpheus: C | '' = '';
 
     constructor(
-        template: TemplateRef<C> | null | undefined,
-        cdr: ChangeDetectorRef | null | undefined,
-    ) {
-        this.template = template ?? inject(TemplateRef<C>, {self: true});
-        this.cdr = cdr ?? inject(ChangeDetectorRef);
-    }
+        readonly template: TemplateRef<C> = inject(TemplateRef<C>, {self: true}),
+        private readonly cdr: ChangeDetectorRef = inject(ChangeDetectorRef),
+    ) {}
 
     public static ngTemplateContextGuard<T>(
         _dir: PolymorpheusTemplate<T>,
