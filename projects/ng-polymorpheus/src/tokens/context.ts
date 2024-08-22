70341344860f7314dd5dc618f2dd6a19aefc186c
splincode <omaxphp@yandex.ru>
2024-08-22 19:21:45 +0300

    feat: add `injectContext`

--- projects/ng-polymorpheus/src/tokens/context.ts
+++ projects/ng-polymorpheus/src/tokens/context.ts
@@ -1,9 +1,14 @@
-import {InjectionToken} from '@angular/core';
+import type {InjectOptions} from '@angular/core';
+import {inject, InjectionToken} from '@angular/core';
 
 /**
  * Use this token to access context within your components when
  * instantiating them through {@link PolymorpheusOutlet}
  */
-export const POLYMORPHEUS_CONTEXT = new InjectionToken<Record<any, any>>(
-    'POLYMORPHEUS_CONTEXT',
-);
+export const POLYMORPHEUS_CONTEXT = new InjectionToken<Record<any, any>>('');
+
+export function injectContext<T>(options?: InjectOptions & {optional?: false}): T;
+export function injectContext<T>(options?: InjectOptions & {optional: true}): T | null;
+export function injectContext<T>(options: InjectOptions = {}): T | null {
+    return inject(POLYMORPHEUS_CONTEXT, options);
+}
