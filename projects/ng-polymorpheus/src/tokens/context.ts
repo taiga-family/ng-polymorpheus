8d34a621ab87d49b597a4fe83ea696e16c1c3e58
mdlufy <german.panov.2018@mail.ru>
2025-06-30 17:30:43 +0300

    chore(ng-polymorpheus): add description to injection tokens

--- projects/ng-polymorpheus/src/tokens/context.ts
+++ projects/ng-polymorpheus/src/tokens/context.ts
@@ -1,3 +1,4 @@
+/// <reference types="@taiga-ui/tsconfig/ng-dev-mode" />
 import type {InjectOptions} from '@angular/core';
 import {inject, InjectionToken} from '@angular/core';
 
@@ -5,7 +6,9 @@ import {inject, InjectionToken} from '@angular/core';
  * Use this token to access context within your components when
  * instantiating them through {@link PolymorpheusOutlet}
  */
-export const POLYMORPHEUS_CONTEXT = new InjectionToken<Record<any, any>>('');
+export const POLYMORPHEUS_CONTEXT = new InjectionToken<Record<any, any>>(
+    ngDevMode ? 'POLYMORPHEUS_CONTEXT' : '',
+);
 
 export function injectContext<T>(options?: InjectOptions & {optional?: false}): T;
 export function injectContext<T>(options?: InjectOptions & {optional: true}): T | null;
